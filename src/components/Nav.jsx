8526aa26ed0035a9e7cdfa8d8fbb1bb451dd8d70
BushCrescent <martian.halo@gmail.com>
2023-06-17 20:05:32 -0700

    asd

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,30 +1,38 @@
-import {
-  Link
-} from "react-router-dom";
-const Nav = () =>{
-    return (
-        <nav className ="navbar navbar-expand-lg navbar-dark bg-dark">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="#">John Park</a>
+import { Link } from "react-router-dom";
+const Nav = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          John Park
+        </a>
 
-    <div id="navbarColor02">
-      <ul className="navbar-nav me-auto">
-            <li className = "nav-item">
-              <Link to="/" className="nav-items">Home</Link>
+        <div id="navbarColor02">
+          <ul className="navbar-nav me-auto">
+            <li className="nav-item">
+              <Link to="/" className="nav-items">
+                Home
+              </Link>
             </li>
-            {/* <li className = "nav-item">
-              <Link to="/AboutMe" className="nav-items">About Me</Link>
-            </li> */}
-            
+            <li className="nav-item">
+              <Link to="/" className="nav-items">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/AboutMe" className="nav-items">
+                About Me
+              </Link>
+            </li>
+
             {/* <li className = "nav-item">
               <Link to="/Form" className="nav-items">Form</Link>
             </li> */}
-      </ul>
-    </div>
-  </div>
-</nav>
-
-    );
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Nav;
